27c44e060155ff423fec56d3518f969d82b4b36e
Nandhups <nandhanaps200@gmail.com>
2025-03-18 18:27:41 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from './pages/home/home.component';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'myproject';
+}
